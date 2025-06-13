da279edb555df0138108127ac01b652627037f1c
Fred Jan Kraan <fjkraan@electrickery.nl>
2025-06-13 13:13:16 +0200

    Added filter for dates and version numbers.

--- tests/ruud6800Test/test.sh
+++ tests/ruud6800Test/test.sh
@@ -15,11 +15,18 @@ $EXE $TESTFILE.asm -Sout/$TESTFILE.s19 -Lout/$TESTFILE.lst > out/a09out.log 2>&1
 DIFFOUND=0
 for FILE in $FILES
 do
-    diff -w out/$FILE ref/$FILE >> out/$LOG
+#    diff -w out/$FILE ref/$FILE >> out/$LOG
+    OUTTEMP=`mktemp /tmp/test_out_temp.XXXXXX`
+    REFTEMP=`mktemp /tmp/test_ref_temp.XXXXXX`
+    sed -f sedfile.txt out/$FILE > $OUTTEMP
+    sed -f sedfile.txt ref/$FILE > $REFTEMP
+    diff -w $OUTTEMP $REFTEMP >> out/$LOG
     if [ $? != 0 ] 
     then
         DIFFOUND=-1
         echo "Difference in $FILE"
+    else
+        rm -f $OUTTEMP $REFTEMP
     fi
 done
 
